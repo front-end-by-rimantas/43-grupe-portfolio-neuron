bf1167bff34bc0fd2170c4502948eb07fbe0799f
Edvinas <edvingvozdovic@gmail.com>
2023-04-29 14:21:23 +0300

    headjs_smoothtransition_improvement

--- js/components/sticky_header.js
+++ js/components/sticky_header.js
@@ -1,15 +1,17 @@
 const meniu = document.querySelector("#meniukas");
+const meniupad = document.querySelector("#head_1");
 
 function sticky_nav() {
 
     if (window.scrollY < 110) {
+        head_1.style.paddingBottom = 0;
         meniukas.classList.remove('head_row_sticky');
-        document.body.style.paddingTop = 0;
+        
 
     } else {
-        meniukas.classList.add('head_row_sticky');
-        document.body.style.transition = 'padding-top 0.4s ease';
-        document.body.style.paddingTop = meniu.offsetHeight + 'px';
+        head_1.style.transition = 'padding-top 0.7s ease';
+        head_1.style.paddingBottom = meniu.offsetHeight + 'px';
+        meniukas.classList.add('head_row_sticky'); 
     }
 }
 window.addEventListener('scroll', sticky_nav);
